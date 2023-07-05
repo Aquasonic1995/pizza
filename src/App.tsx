5948a46c6f1e9025c856523082ef96067feebe4a
Aquasonic1995 <Aquasonic1995@gmail.com>
2023-07-05 09:31:41 +0300

    add markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './scss/app.scss';
+import pizzaLogo from './img/pizza-logo.svg'
+import Header from "./Components/Header/Header";
+import Categories from "./Components/Categories/Categories";
+import Sort from "./Components/Sort/Sort";
+import PizzaBlock from "./Components/PizzaBlock/PizzaBlock";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsxxxxxxxx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (<div className="wrapper">
+      <Header/>
+        <div className="content">
+            <div className="container">
+                <div className="content__top">
+                   <Categories/>
+                <Sort/>
+                </div>
+                <h2 className="content__title">Все пиццы</h2>
+                <div className="content__items">
+
+                    <PizzaBlock/>
+                    <PizzaBlock/>
+                    <PizzaBlock/>
+                    <PizzaBlock/>
+
+                </div>
+            </div>
+        </div>
+    </div>);
 }
 
 export default App;
